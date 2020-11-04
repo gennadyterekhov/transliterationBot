bb5eaf79c41f36b4543b7bd53637c1871373b91a
Gennady Terekhov <ter.ge@yandex.ru>
2020-11-05 02:47:07 +0300

    now works through api at golang-react-transliterator.heroluapp.com

--- helper/helper.go
+++ helper/helper.go
@@ -1,7 +1,49 @@
 package helper
 
+import "encoding/json"
+
+type RequestJson struct {
+	From string `json:"from"`
+	To   string `json:"to"`
+	Text string `json:"text"`
+}
+
+type ResponseJson struct {
+	Ok     bool   `json:"ok"`
+	From   string `json:"from"`
+	To     string `json:"to"`
+	Result string `json:"result"`
+}
+
 func Check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+func GetRequestJson(jsonString []byte) RequestJson {
+	var requestStruct RequestJson = RequestJson{}
+	if err := json.Unmarshal(jsonString, &requestStruct); err != nil {
+		panic(err)
+	}
+	return requestStruct
+}
+
+func GetResponseJson(jsonString []byte) ResponseJson {
+	var responseStruct ResponseJson = ResponseJson{}
+	if err := json.Unmarshal(jsonString, &responseStruct); err != nil {
+		panic(err)
+	}
+	return responseStruct
+}
+func GenerateResponseBody(from string, to string, result string) string {
+	var responseStruct ResponseJson = ResponseJson{
+		Ok:     true,
+		From:   from,
+		To:     to,
+		Result: result,
+	}
+	resultBytes, err := json.Marshal(responseStruct)
+	Check(err)
+	var resultString string = string(resultBytes)
+	return resultString
 }
--- transliterate/transliterate.go
+++ transliterate/transliterate.go
@@ -1,48 +1,77 @@
 package transliterate
 
-import "os"
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"transliteration_bot/helper"
+)
+
+func transliterateFromApi(text string) string {
+	var requestStruct helper.RequestJson = helper.RequestJson{
+		From: "cyrillic",
+		To:   "latin",
+		Text: text,
+	}
+
+	requestStructBytes, err := json.Marshal(requestStruct)
+	helper.Check(err)
+	// var requestStructString string = string(requestStructBytes)
+
+	response, err := http.Post(os.Getenv("transliterator_api_url"), "application/json", bytes.NewBuffer(requestStructBytes))
+	helper.Check(err)
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+
+	var resposeStruct helper.ResponseJson = helper.GetResponseJson(body)
+	return resposeStruct.Result
+}
 
 func Transliterate(msg string) string {
 	// local
 	// var cyrillic string = config.GetConfig().Cyrillic
 	// var latin string = config.GetConfig().Latin
 	// prod
-	var cyrillic string = os.Getenv("cyrillic")
-	var latin string = os.Getenv("latin")
+	return transliterateFromApi(msg)
+	// var cyrillic string = os.Getenv("cyrillic")
+	// var latin string = os.Getenv("latin")
 
-	const numberOfChars int = 66
+	// const numberOfChars int = 66
 
-	var cyrillicRunes [numberOfChars]rune
-	var latinRunes [numberOfChars]rune
-	var trans string = ""
+	// var cyrillicRunes [numberOfChars]rune
+	// var latinRunes [numberOfChars]rune
+	// var trans string = ""
 
-	var correspondence map[rune]rune = make(map[rune]rune)
+	// var correspondence map[rune]rune = make(map[rune]rune)
 
-	var j uint8 = 0
-	for _, runeValue := range cyrillic {
-		cyrillicRunes[j] = runeValue
-		j += 1
-	}
-	j = 0
-	for _, runeValue := range latin {
-		latinRunes[j] = runeValue
-		j += 1
-	}
+	// var j uint8 = 0
+	// for _, runeValue := range cyrillic {
+	// 	cyrillicRunes[j] = runeValue
+	// 	j += 1
+	// }
+	// j = 0
+	// for _, runeValue := range latin {
+	// 	latinRunes[j] = runeValue
+	// 	j += 1
+	// }
 
-	for i := 0; i < numberOfChars; i += 1 {
-		correspondence[cyrillicRunes[i]] = latinRunes[i]
-	}
+	// for i := 0; i < numberOfChars; i += 1 {
+	// 	correspondence[cyrillicRunes[i]] = latinRunes[i]
+	// }
 
-	for _, runeValue := range msg {
-		if latinChar, ok := correspondence[runeValue]; ok {
-			//do something here
-			trans += string(latinChar)
-		} else {
-			trans += string(runeValue)
-		}
+	// for _, runeValue := range msg {
+	// 	if latinChar, ok := correspondence[runeValue]; ok {
+	// 		//do something here
+	// 		trans += string(latinChar)
+	// 	} else {
+	// 		trans += string(runeValue)
+	// 	}
 
-	}
+	// }
 
-	return trans
+	// return trans
 
 }
